67f930f56d458670972f63f16a4ad59e7a2302c5
Jihun-Kim <anam4561@naver.com>
2023-04-15 04:28:22 +0900

    fix : ui 수정

--- ProductExchangeService/ui/ServiceUI.java
+++ ProductExchangeService/ui/ServiceUI.java
@@ -24,12 +24,12 @@ public class ServiceUI {
             String command = input[0];
 
             // 고객 요청별 분기처리
-            // 1. 문의
             switch (command.toLowerCase()) {
+                // 1. 문의
                 case "help":
                     // help 이외의 문자/숫자가 포함된 경우 에러 메시지
                     if (input.length != 1) productExchangeService.errorService(1);
-                        // 아니라면 helpService
+                    // 아니라면 helpService
                     else {
                         productExchangeService.helpService();
                     }
@@ -72,38 +72,34 @@ public class ServiceUI {
                         inputStr += input[i];
                     }
 
+                    // 상점코드 6자리 + 상품코드 9자리 총 15자리가 아니라면 에러메시지
                     if(inputStr.length()!= 15) productExchangeService.errorService(3);
                     else {
+                        // 상점 코드 6자리
                         storeCodeStr = inputStr.substring(0,6);
+                        //상품 코드 9자리
                         productCodeStr = inputStr.substring(6);
 
-                        boolean numCheck = true;
 
+                        // [1] 상품코드 확인
                         // 상품 코드에 숫자가 아닌 문자가 섞여있는지 확인
                         for (int i = 0; i < productCodeStr.length(); i++) {
                             if (!Character.isDigit(productCodeStr.charAt(i))) {
-                                numCheck = false;
+                                productExchangeService.errorService(3);
+                                return;
                             }
                         }
-                        // 문자가 섞여있다면 에러 메시지 후 return
-                        if (!numCheck) {
-                            productExchangeService.errorService(3);
-                            return;
-                        }
-                        boolean charCheck = true;
+
+                        // [2] 상점코드 확인
                         // 상점 코드에 영어가 아닌 문자가 섞여있는지 확인
                         for (int i = 0; i < storeCodeStr.length(); i++) {
-                            if (storeCodeStr.charAt(i) < 'A' && storeCodeStr.charAt(i) > 'z') charCheck = false;
+                            if (storeCodeStr.charAt(i) < 'A' || storeCodeStr.charAt(i) > 'z') {
+                                productExchangeService.errorService(3);
+                                return;
+                            }
                         }
                         // 영어만 있다면 claimService 실행
-                        if (charCheck) {
-                            productExchangeService.claimService(productCodeStr, storeCodeStr);
-                        }
-                        // 영어가 아닌 문자가 섞여있다면 에러 메시지
-                        else {
-                            productExchangeService.errorService(3);
-                            return;
-                        }
+                        productExchangeService.claimService(productCodeStr, storeCodeStr);
                     }
                     break;
                 // 4. 프로그램 종료 코드
